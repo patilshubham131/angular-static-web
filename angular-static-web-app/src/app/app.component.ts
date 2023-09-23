8bf2975f52a890dfe6e583e40f705b14f8ebb3ec
patilshubham131 <76581445+patilshubham131@users.noreply.github.com>
2023-09-23 13:32:40 +0530

    Update app.component.ts

--- angular-static-web-app/src/app/app.component.ts
+++ angular-static-web-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-static-web-app';
+  title = 'angular-static-web-app BY SHUBHAM PATIL';
 }
